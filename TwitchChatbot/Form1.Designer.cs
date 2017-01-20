84e6d4b385ff93061cd5dbb95cffaef19a9cbba4
Mordenak <nicksolwick@gmail.com>
2017-01-19 18:15:07 -0800

    Initial commit

---
+++ TwitchChatbot/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace TwitchChatbot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chatDisplay = new System.Windows.Forms.RichTextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtChatroom = new System.Windows.Forms.TextBox();
+            this.labelChatroom = new System.Windows.Forms.Label();
+            this.labelConnectedChatroom = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chatDisplay
+            // 
+            this.chatDisplay.Location = new System.Drawing.Point(41, 45);
+            this.chatDisplay.Name = "chatDisplay";
+            this.chatDisplay.Size = new System.Drawing.Size(313, 233);
+            this.chatDisplay.TabIndex = 0;
+            this.chatDisplay.Text = "";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(43, 287);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(152, 40);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(201, 287);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(152, 39);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // txtChatroom
+            // 
+            this.txtChatroom.Location = new System.Drawing.Point(42, 336);
+            this.txtChatroom.Name = "txtChatroom";
+            this.txtChatroom.Size = new System.Drawing.Size(152, 20);
+            this.txtChatroom.TabIndex = 3;
+            // 
+            // labelChatroom
+            // 
+            this.labelChatroom.AutoSize = true;
+            this.labelChatroom.Location = new System.Drawing.Point(207, 338);
+            this.labelChatroom.Name = "labelChatroom";
+            this.labelChatroom.Size = new System.Drawing.Size(52, 13);
+            this.labelChatroom.TabIndex = 4;
+            this.labelChatroom.Text = "Chatroom";
+            // 
+            // labelConnectedChatroom
+            // 
+            this.labelConnectedChatroom.AutoSize = true;
+            this.labelConnectedChatroom.Location = new System.Drawing.Point(40, 20);
+            this.labelConnectedChatroom.Name = "labelConnectedChatroom";
+            this.labelConnectedChatroom.Size = new System.Drawing.Size(0, 13);
+            this.labelConnectedChatroom.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 436);
+            this.Controls.Add(this.labelConnectedChatroom);
+            this.Controls.Add(this.labelChatroom);
+            this.Controls.Add(this.txtChatroom);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.chatDisplay);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox chatDisplay;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox txtChatroom;
+        private System.Windows.Forms.Label labelChatroom;
+        private System.Windows.Forms.Label labelConnectedChatroom;
+    }
+}
+
---
+++ TwitchChatbot/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TwitchLib;
+using TwitchLib.Models.API;
+using TwitchLib.Models.Client;
+using TwitchLib.Events.Client;
+using TwitchLib.Exceptions.API;
+using TwitchLib.Events.PubSub;
+using TwitchLib.Events.Services.FollowerService;
+using TwitchLib.Events.Services.MessageThrottler;
+using TwitchLib.Enums;
+using TwitchLib.Extensions.Client;
+
+namespace TwitchChatbot
+{
+    public partial class Form1 : Form
+    {
+        public TwitchClient client = new TwitchClient(new TwitchLib.Models.Client.ConnectionCredentials(Properties.Settings.Default.username, Properties.Settings.Default.oauth));
+        public String lastChatroom = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            client.OnConnected += new EventHandler<OnConnectedArgs>(onConnected);
+            client.OnDisconnected += new EventHandler<OnDisconnectedArgs>(onDisconnected);
+            client.OnMessageReceived += new EventHandler<OnMessageReceivedArgs>(globalChatMessageReceived);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (!client.IsConnected && txtChatroom.Text != "")
+            {
+                client.Connect();
+
+                updateChatDisplay("Connecting...");
+            }
+            else if (txtChatroom.Text == "")
+            {
+                updateChatDisplay("Please enter a chatroom.");
+            }
+            else
+            {
+                updateChatDisplay("Already connected.");
+            }
+            
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+            {
+                if (lastChatroom != "")
+                {
+                    client.LeaveChannel(lastChatroom);
+                }
+                client.Disconnect();
+
+                updateChatDisplay("Disconnecting...");
+            }
+            else
+            {
+                updateChatDisplay("Not connected.");
+            }
+        }
+
+        public void onConnected(object sender, OnConnectedArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            client.JoinChannel(txtChatroom.Text);
+            lastChatroom = txtChatroom.Text;
+
+            updateChatDisplay("<< Connected to " + txtChatroom.Text + " chat server >>");
+            updateChatLabel(txtChatroom.Text);
+            
+        }
+
+        public void onDisconnected(object sender, OnDisconnectedArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            updateChatDisplay("<< Disconnected from chat >>");
+            updateChatLabel("");
+        }
+
+        public void globalChatMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            updateChatDisplay(e.ChatMessage.Username + ": " + e.ChatMessage.Message);
+        }
+
+        public void updateChatDisplay(string message)
+        {
+            chatDisplay.Text = chatDisplay.Text + "\n" + message;
+            chatDisplay.SelectionStart = chatDisplay.Text.Length;
+            chatDisplay.ScrollToCaret();
+        }
+
+        public void updateChatLabel(string message)
+        {
+            if (message != "")
+            {
+                labelConnectedChatroom.Text = message + "'s chat";
+            }
+            else
+            {
+                labelConnectedChatroom.Text = "";
+            }
+            
+        }
+
+    }
+}
